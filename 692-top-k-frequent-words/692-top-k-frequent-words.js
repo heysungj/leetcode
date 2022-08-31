fd9d25cc70b2f3534aa906333b737bcc166d1197
Jing Lin <66035326+heysungj@users.noreply.github.com>
2022-08-30 22:25:08 -0400

    Time: 173 ms (6.63%), Space: 48.8 MB (12.97%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.js
+++ 692-top-k-frequent-words/692-top-k-frequent-words.js
@@ -5,7 +5,6 @@
  */
 var topKFrequent = function(words, k) {
      const map1 = new Map();
-    let result = []
     if(words.length === 1) return words
     for(let i=0;i<words.length; i++) {
         if(!map1.get(words[i])){
@@ -14,27 +13,9 @@ var topKFrequent = function(words, k) {
             map1.set(words[i], map1.get(words[i])+1)
         }
     }
-    // console.log(map1)
-    // let sortable = []
-    // map1.forEach ((value, key) => {
-    //     sortable.push([key,value])
-    //     }
-    //  )   
-    
-    
         
     let temp = [...map1.keys()].sort((a,b) =>  map1.get(b) === map1.get(a) ? a.localeCompare(b) : map1.get(b) - map1.get(a))
     console.log(temp);
     return (temp.slice(0,k))
    
 };
-    
-//     let sorted = sortable.sort((a,b) => b[1]-a[1])
-//     // console.log(sorted)
-//     let temp = sorted.slice(0,k)
-//     for(let j=0; j<k; j++) {
-//         result.push(temp[j][0])
-//     }
-    
-//     return result
-// };
