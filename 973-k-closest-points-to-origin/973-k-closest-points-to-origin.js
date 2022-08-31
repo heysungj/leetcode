2e91433dc5a06d3ba1865a434396001f5fab4271
Jing Lin <66035326+heysungj@users.noreply.github.com>
2022-08-30 21:40:00 -0400

    Time: 370 ms (38.07%), Space: 60.2 MB (69.58%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -9,14 +9,14 @@ var kClosest = function(points, k) {
     let distance;
     for(let i=0; i<points.length; i++) {
         distance = Math.pow(points[i][0], 2) + Math.pow(points[i][1],2)
-        tempResult.push([distance, points[i]])
+        tempResult.push([distance, i])
     }
     
     tempResult.sort((a,b)=> a[0]-b[0] )
  
     let temp = tempResult.slice(0,k)
     for(let j=0; j<k; j++) {
-        outPut.push(tempResult[j][1])
+        outPut.push(points[tempResult[j][1]])
     }
     
     return outPut
