b959d56458336edcc15ec0d757b6eff3e50f614f
Jing Lin <66035326+heysungj@users.noreply.github.com>
2022-09-01 23:01:40 -0400

    Time: 154 ms (33.70%), Space: 54.4 MB (28.62%) - LeetHub

---
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.js
@@ -0,0 +1,77 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @constructor
+ * @param {NestedInteger[]} nestedList
+ */
+var NestedIterator = function(nestedList) {
+    this.nestedList = nestedList
+    this.result = []
+    this.index = 0
+    // console.log(this.nestedList.length)
+    function flattern(list,res){
+        let position = 0
+        while(position < list.length){
+            if(list[position].isInteger()){
+            res.push(list[position].getInteger())
+            } else {
+                flattern(list[position].getList(), res)
+            };
+            position++
+        }  
+    }
+    
+    flattern(this.nestedList, this.result )
+    // console.log(this.result)
+    
+};
+
+
+/**
+ * @this NestedIterator
+ * @returns {boolean}
+ */
+NestedIterator.prototype.hasNext = function() {
+    return (this.index < this.result.length) 
+        
+};
+
+/**
+ * @this NestedIterator
+ * @returns {integer}
+ */
+NestedIterator.prototype.next = function() {
+    let element = this.result[this.index] 
+    this.index++
+    return element
+};
+
+/**
+ * Your NestedIterator will be called like this:
+ * var i = new NestedIterator(nestedList), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
