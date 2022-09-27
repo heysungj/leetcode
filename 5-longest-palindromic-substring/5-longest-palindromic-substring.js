508f0ff8b75b5e1939f0cef75fea6f649eeab02c
Jing Lin <66035326+heysungj@users.noreply.github.com>
2022-09-27 00:11:15 -0400

    Time: 309 ms (36.10%), Space: 48.9 MB (25.79%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -2,38 +2,19 @@
  * @param {string} s
  * @return {string}
  */
-// var longestPalindrome = function(s) {
-//     let lIdx = 0;
-//     let rIdx = 0;
-//     let l , r;
+var longestPalindrome = function(s) {
+    let lIdx = 0;
+    let rIdx = 0;
+    // let l , r;
     
-//     for(let i = 0; i < s.length; i++) {
-//         for(let j = i; j <= i+1; j++) {
-//             for( l = i, r = j; s[l] && s[l] === s[j]; l--, r++){
-//                 [lIdx, rIdx] = rIdx - lIdx > r - l? [lIdx, rIdx] : [l, r];
-//             }
-//         }
-//     }
-    
-//     return s.substring(lIdx, rIdx)
-//     };
-
-function longestPalindrome(s) {
-  // ll: Left index of the longest palindrome.
-  // rr: Right index of the longest palindrome.
-  let ll = 0, rr = 0;
-
-  // Iterate all palindromes with center indices
-  // [..., i, ...] or [... i, i+1, ...]
-  for (let i = 0; i < s.length; i++) {
-    for (let j of [i, i+1]) {
-      for (l = i, r = j; s[l] && s[l] === s[r]; l--, r++)
-
-        // Found a new palindrome [l, ..., i, j, ..., r]
-        // Update the ll, rr if the newly found palindrome is longer than the
-        // existing one.
-        [ll, rr] = (r-l+1) > (rr-ll+1) ? [l, r] : [ll, rr];
+    for(let i = 0; i < s.length; i++) {
+        for(let j = i; j <= i+1; j++) {
+            for(let l = i, r = j; s[l] && s[l] === s[r]; l--, r++){
+                [lIdx, rIdx] = rIdx - lIdx > r - l? [lIdx, rIdx] : [l, r];
+            }
+        }
     }
-  }
-  return s.substring(ll, rr+1);
-}
+    
+    return s.substring(lIdx, rIdx+1)
+    };
+
